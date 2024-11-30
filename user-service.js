6890de1415106765176bc639cbf6e6d4062e9513
klousable <carrieleung@live.ca>
2024-11-30 16:01:39 -0500

    test

--- user-service.js
+++ user-service.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
 dotenv.config();
 
-let mongoDBConnectionString = `${process.env.MONGO_URL}/${process.env.MONGO_DB}`;
+let mongoDBConnectionString = `${process.env.MONGO_URL}`;
 let Schema = mongoose.Schema;
 
 let userSchema = new Schema({
@@ -20,16 +20,20 @@ let User;
 
 module.exports.connect = function () {
   return new Promise(function (resolve, reject) {
-    let db = mongoose.createConnection(mongoDBConnectionString);
-
-    db.on("error", (err) => {
-      reject(err);
-    });
-
-    db.once("open", () => {
-      User = db.model("users", userSchema);
-      resolve();
-    });
+    // Pass the database name in the options instead of in the URL
+    mongoose
+      .connect(mongoDBConnectionString, {
+        dbName: process.env.MONGO_DB,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        User = mongoose.model("users", userSchema);
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
 };
 
