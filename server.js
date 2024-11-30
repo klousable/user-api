1a614ce099b8e773d331021a7a6ff3cd7e125d4b
klousable <carrieleung@live.ca>
2024-11-29 22:36:27 -0500

    test

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(passport.initialize());
 
 app.post("/api/user/register", (req, res) => {
-  console.log("Registration request data:", req.body);
   userService
     .registerUser(req.body)
     .then((msg) => {
